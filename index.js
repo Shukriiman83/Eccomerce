6edd2d4e2661539097be34b71c153365896c63c1
Shukri Mohamed <Shukriiman83@gmail.com>
2025-02-02 22:28:37 +0800

    added toggle and activated search bar

--- index.js
+++ index.js
@@ -1,51 +1,98 @@
+let productsData = [];
 
-let productsData=[]
+const data = fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((json) => {
+    productsData = json;
+    displayProducts(productsData);
+  });
 
-const data=fetch('https://fakestoreapi.com/products')
-.then(res=>res.json())
-.then(json=>{
-    productsData=json;
-    displayProducts()
-});
+function displayProducts(productsData) {
+  const select = document.querySelector("input");
+  productContainer.innerHTML = "";
+  select.addEventListener("input", function (e) {
+    const userInput = e.target.value;
+    if(userInput){
+    const filteredData = productsData.filter((item) =>
+      item.title.toLowerCase().includes(userInput)
+    );
+    displayProducts(filteredData);
+  }
+  else{
+    displayProducts(productsData)
+  }
+  });
+/**  toggle menu */
 
-function displayProducts(){
+  const toggle=document.querySelector('.togglebtn')
+  const menu=document.querySelector('.menu')
+toggle.addEventListener('click',function(e){
+  menu.classList.toggle('active');
+  menu.classList.toggle('hidden');
+})
 
-  const select=document.querySelector('input')
-  select.addEventListener('input',function(e){
-const userInput=e.target.value;
-const filteredData=productsData.filter((item)=>item.title.toLowerCase().includes(userInput))
-console.log(filteredData)
-  })
-    const container=document.getElementById('productContainer');
-    productsData.map((product)=>{
-        const productElement=document.createElement('div')
-        productElement.classList.add('p-6', 'bg-white', 'rounded-lg', 'shadow-lg', 'max-w-xs', 'mx-auto');
 
-      //1 img
-      const imgdiv = document.createElement('div')
-      imgdiv.classList.add('p-2','h-auto', 'bg-white', 'rounded-lg', 'shadow-lg')
-      const img=document.createElement('img')
-      img.src=product.image
-      img.classList.add('w-full', 'h-40', 'object-contain', 'rounded-md', 'shadow-md')
-    imgdiv.appendChild(img)
-      productElement.appendChild(imgdiv)
 
-      //2 title
-      const title=document.createElement('h2')
-      title.classList.add('text-xl', 'font-semibold' ,'text-gray-600', "mt-4")
-      title.textContent=product.title
-      productElement.appendChild(title)
-//3 price
-const price=document.createElement('p')
-price.textContent=product.price
-price.classList.add("text-lg","font-bold", "text-gray-900", "mt-2")
-productElement.appendChild(price)
-//4 btn
-const btn=document.createElement('button')
-btn.textContent='Add to cart '
-btn.classList.add("bg-blue-500", "text-white", "py-2", "px-4", "rounded-md", "mt-4", "hover:bg-blue-600", "focus:outline-none", "focus:ring-2", "focus:ring-blue-300")
-productElement.appendChild(btn)
-container.appendChild(productElement)
-    })
+/**  container */
+  const container = document.getElementById("productContainer");
+  productsData.map((product) => {
+    const productElement = document.createElement("div");
+    productElement.classList.add(
+      "p-6",
+      "bg-white",
+      "rounded-lg",
+      "shadow-lg",
+      "max-w-xs",
+      "mx-auto"
+    );
+
+    //1 img
+    const imgdiv = document.createElement("div");
+    imgdiv.classList.add(
+      "p-2",
+      "h-auto",
+      "bg-white",
+      "rounded-lg",
+      "shadow-lg"
+    );
+    const img = document.createElement("img");
+    img.src = product.image;
+    img.classList.add(
+      "w-full",
+      "h-40",
+      "object-contain",
+      "rounded-md",
+      "shadow-md"
+    );
+    imgdiv.appendChild(img);
+    productElement.appendChild(imgdiv);
+
+    //2 title
+    const title = document.createElement("h2");
+    title.classList.add("text-xl", "font-semibold", "text-gray-600", "mt-4");
+    title.textContent = product.title;
+    productElement.appendChild(title);
+    //3 price
+    const price = document.createElement("p");
+    price.textContent = product.price;
+    price.classList.add("text-lg", "font-bold", "text-gray-900", "mt-2");
+    productElement.appendChild(price);
+    //4 btn
+    const btn = document.createElement("button");
+    btn.textContent = "Add to cart ";
+    btn.classList.add(
+      "bg-blue-500",
+      "text-white",
+      "py-2",
+      "px-4",
+      "rounded-md",
+      "mt-4",
+      "hover:bg-blue-600",
+      "focus:outline-none",
+      "focus:ring-2",
+      "focus:ring-blue-300"
+    );
+    productElement.appendChild(btn);
+    container.appendChild(productElement);
+  });
 }
-
